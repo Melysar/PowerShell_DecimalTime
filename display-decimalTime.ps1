5083b1ac46b5a011f5d0aa117982d0e6e3eacbd7
Melysar <37706144+Melysar@users.noreply.github.com>
2019-02-04 08:19:54 -0700

    Initial Upload
    uploaded file: 'display-decimalTime.ps1'

---
+++ display-decimalTime.ps1
@@ -0,0 +1,15 @@
+﻿while ($true)
+{
+    Clear-Host
+    $date = Get-Date
+    Write-Host "$($date.Hour):$($date.Minute):$($date.Millisecond)"
+    $currentTimeInSeconds = ($date.Hour * 3600) + ($date.Minute * 60) + $date.Second + ($date.Millisecond / 1000)
+    write-host $currentTimeInSeconds
+    $currentTimeInDecimal = $currentTimeInSeconds / 86.4
+    $currentTimeInDecimal = [math]::Round($currentTimeInDecimal,2)
+    $decimalDayPercentage = $currentTimeInDecimal / 10
+
+    Write-Host "Current Decimal Time: ${currentTimeInDecimal} / 1000"
+    Write-Host "${decimalDayPercentage}% of Day Complete"
+    Start-Sleep -Milliseconds 250
+}
